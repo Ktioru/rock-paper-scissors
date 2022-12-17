277947b6fa69f12458f66b831b7aaa87cab14664
Ktioru <mateuscst45@gmail.com>
2022-12-17 19:22:39 -0300

    Add function to play the game for one round

--- script.js
+++ script.js
@@ -18,7 +18,42 @@ function getComputersChoice() {
     
 }
 
+function playOneRound(playersChoice, computersChoice) {
+    if (playersChoice == "rock") {
+        if (computersChoice == "rock") {
+            console.log("It's a tie!")
 
+        } else if (computersChoice == "paper") {
+            console.log("You lose! Paper beats rock!")
+
+        } else if (computersChoice == "scissors") {
+            console.log("You Win! Rock beats scissors!")
+
+        }
+    }  else if (playersChoice == "paper") {
+        if (computersChoice == "paper") {
+            console.log("It's a tie!")
+
+        } else if (computersChoice == "rock") {
+            console.log("You Win! Paper beats rock!")
+
+        } else if (computersChoice == "scissors") {
+            console.log("You Lose! Scissors beats paper!")
+
+        }
+    }  else if (playersChoice == "scissors") {
+        if (computersChoice == "scissors") {
+            console.log("It's a tie!")
+
+        } else if (computersChoice == "rock") {
+            console.log("You Lose! Rock beats scissors!")
+
+        } else if (computersChoice == "paper") {
+            console.log("You Win! Scissors beats paper!")
+
+        }
+    } 
+}
 
 
 
